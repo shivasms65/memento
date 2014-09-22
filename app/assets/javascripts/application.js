9d4334ab500df23370cf69fa9ddcecbd864ce999
sivakumar <shiva.sms65@gmail.com>
2014-09-22 23:25:53 +0530

    changes for datatables responsive

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,4 @@
 //= require jquery
 //= require jquery_ujs
 //= require dataTables/jquery.dataTables
-//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
+//= require dataTables/extras/dataTables.tableTools
