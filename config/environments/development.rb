2406304e20e1f410d0ee3511784c9ad5ec02fc24
sivakumar <shiva.sms65@gmail.com>
2014-09-24 00:12:10 +0530

    mailer settings changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Memento::Application.configure do
       port:                 587,
       domain:               'gmail.com',
       user_name:            'genetech003@gmail.com',
-      password:             'beindian',
+      password:             'beindian123',
       authentication:       'plain'
       # enable_starttls_auto: true
   }
